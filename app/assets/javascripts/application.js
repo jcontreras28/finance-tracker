a0de2cdace65bc10a46cf120fa12c990696f7693
Janelle Contreras <janellec@bendbroadband.com>
2017-12-03 00:54:27 +0000

    backup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,8 @@ $(document).ready(function(){
   $('#stock-lookup-form').on('ajax:complete', function(event, data, status){
     $('#results').html(data.responseText);
   });
+  
+  $('#friend-lookup-form').on('ajax:complete', function(event, data, status){
+    $('#results').html(data.responseText);
+  });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
   end
   
   def my_friends
+    @friendships = current_user.friends
+  end
+  
+  def search
+    if (params[:search_param].blank?)
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users match this search criteria" if @users.blank?
+    end
+    render partial: 'friends/result'
   end
   
 end
